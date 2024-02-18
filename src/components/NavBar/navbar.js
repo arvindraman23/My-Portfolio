4fa9951e0611645e44fd36f015d35a2082e76a31
arvindraman23 <arvindraman23@gmail.com>
2024-02-18 03:51:48 -0600

    UI Changes

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -14,7 +14,6 @@ const Navbar = () => {
                 <Link activeClass='active' to='intro' spy={true} offset={-100} duration={500} className='desktopMenuListItem'>Home</Link>
                 <Link activeClass='active' to='skills' spy={true} offset={-50} duration={500} className='desktopMenuListItem'>About</Link>
                 <Link activeClass='active' to='works' spy={true} offset={-50} duration={500} className='desktopMenuListItem'>Portfolio</Link>
-                <Link activeClass='active' to='clients' spy={true} offset={-50} duration={500} className='desktopMenuListItem'>Clients</Link>
             </div>
             <button className='desktopMenuBtn' onClick={() => {
                 document.getElementById('contact').scrollIntoView({behavior: 'smooth'});
@@ -26,7 +25,6 @@ const Navbar = () => {
                 <Link activeClass='active' to='intro' spy={true} offset={-100} duration={500} className='listItem' onClick={()=>setShowMenu(false)}>Home</Link>
                 <Link activeClass='active' to='skills' spy={true} offset={-50} duration={500} className='listItem' onClick={()=>setShowMenu(false)}>About</Link>
                 <Link activeClass='active' to='works' spy={true} offset={-50} duration={500} className='listItem' onClick={()=>setShowMenu(false)}>Portfolio</Link>
-                <Link activeClass='active' to='clients' spy={true} offset={-50} duration={500} className='listItem' onClick={()=>setShowMenu(false)}>Clients</Link>
                 <Link activeClass='active' to='contact' spy={true} offset={-50} duration={500} className='listItem' onClick={()=>setShowMenu(false)}>Contact</Link>
             </div>
         </nav>
