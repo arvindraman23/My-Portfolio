08c34d3e5d1896b3d9c2d806bcb039f54728ced8
arvindraman23 <arvindraman23@gmail.com>
2024-02-27 17:20:57 -0600

    test-change

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -8,7 +8,7 @@ const Intro = () => {
     return (
         <section id='intro'>
             <div className='introContent'>
-                <span className='hello'>Hello,</span>
+                <span className='hello'>Hola,</span>
                 <span className='introText'>I'm <span className='introName'>Arvind Raman</span> <br />Software Engineer | iOS Developer</span>
                 <p className='introPara'> I'm a skilled Software Engineer with 3 years of hands-on experience <br />in architecting applications. Proficient at crafting innovative solutions <br />that elevate user experience and drive business growth.</p>
                 <Link><button className='btn' onClick={() => {
